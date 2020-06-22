d6d4f6a461631f6229d9d186a0da1c9d1ba86b10
Julian Calvento <juliancalvento@gmail.com>
2020-06-22 15:43:58 -0300

    - Polite polling for Entez api - Refactor

---
+++ blast.py
@@ -0,0 +1,66 @@
+import os
+
+from Bio.Blast import NCBIXML
+
+
+class Blast:
+    def run(self, input_fasta, db_path):
+        # Blast
+        blast_result = os.popen(f'blastp -query {input_fasta} -db {db_path} -outfmt 5')
+        blast_records = NCBIXML.read(blast_result)
+
+        sequence = []
+        with open(input_fasta, "r") as f:
+            for line in f.readlines():
+                if not line.startswith(">"):
+                    sequence.append(line)
+        protein_chain = "".join(sequence).replace("\n", "")
+
+        # Mejores hits segun criterio
+        filtered_results = list(filter(
+            lambda alignment: self.__meets_expected_criteria(protein_chain, alignment), blast_records.alignments
+        ))
+
+        # Crear Fasta con hits
+        return self.__make_fasta(filtered_results)
+
+    def __evalue(self, alignment):
+        hsp = alignment.hsps[0]
+        if hasattr(hsp, 'evalue'):
+            return hsp.evalue
+        else:
+            return 0
+
+    def __identity_percentage(self, alignment):
+        hsp = alignment.hsps[0]
+        return round(hsp.identities / hsp.align_length, 2)
+
+    def __coverage(self, protein_chain, alignment):
+        hsp = alignment.hsps[0]
+        return ((hsp.query_end - hsp.query_start) / len(protein_chain)) * 100
+
+    def __meets_expected_criteria(self, protein_chain, alignment):
+        return self.__identity_percentage(alignment) >= 0.4 and self.__evalue(alignment) <= 0.005 and \
+               self.__coverage(protein_chain, alignment) >= 90
+
+    # parametrizable pero defaults:
+    # porcentaje de identidad > 40%
+    # coverage > 90% ((dfblst['q.end']-dfblst['q.start'])/query_length)*100.0
+    # evalue < 0.005
+
+    def __fasta_info(self, alignment):
+        accession = alignment.hit_id.split('|')[1]
+        gen_name = alignment.hit_def.split(';')[0].split('RecName: Full=')[-1]
+        raw_species = alignment.hit_def.split(';')[-1]
+        species = raw_species[raw_species.find("[") + 1:raw_species.find("]")]
+        header = f">{accession} {species}, gene for {gen_name}"
+        sequence = alignment.hsps[0].sbjct
+
+        return f"{header}\n{sequence}"
+
+    def __make_fasta(self, alignments):
+        content = "\n\n".join(list(map(lambda alignment: self.__fasta_info(alignment), alignments)))
+        fasta_file = open("blast_result.fasta", "w")
+        fasta_file.write(content)
+        fasta_file.close()
+        return fasta_file.name
--- scripts.py
+++ scripts.py
@@ -1,83 +1,8 @@
-import os
 import subprocess
-
 import requests
-from Bio import Entrez
-from Bio.Blast import NCBIXML
 
-
-def most_similar_between(alignment, another_alignment):
-    if identity_percentage(alignment) >= identity_percentage(another_alignment) or\
-            evalue(alignment) <= evalue(another_alignment) or coverage(alignment) >= coverage(another_alignment):
-        return alignment
-    return another_alignment
-
-
-def evalue(alignment):
-    hsp = alignment.hsps[0]
-    if hasattr(hsp, 'evalue'):
-        return hsp.evalue
-    else:
-        return 0
-
-
-def identity_percentage(alignment):
-    hsp = alignment.hsps[0]
-    return round(hsp.identities / hsp.align_length, 2)
-
-
-def coverage(protein_chain, alignment):
-    hsp = alignment.hsps[0]
-    return ((hsp.query_end - hsp.query_start) / len(protein_chain)) * 100
-
-
-def meets_expected_criteria(protein_chain, alignment):
-    return identity_percentage(alignment) >= 0.4 and evalue(alignment) <= 0.005 and\
-           coverage(protein_chain, alignment) >= 90
-# parametrizable pero defaults:
-# porcentaje de identidad > 40%
-# coverage > 90% ((dfblst['q.end']-dfblst['q.start'])/query_length)*100.0
-# evalue < 0.005
-
-
-def fasta_info(alignment):
-    accession = alignment.hit_id.split('|')[1]
-    gen_name = alignment.hit_def.split(';')[0].split('RecName: Full=')[-1]
-    raw_species = alignment.hit_def.split(';')[-1]
-    species = raw_species[raw_species.find("[") + 1:raw_species.find("]")]
-    header = f">{accession} {species}, gene for {gen_name}"
-    sequence = alignment.hsps[0].sbjct
-
-    return f"{header}\n{sequence}"
-
-
-def make_fasta(alignments):
-    content = "\n\n".join(list(map(lambda alignment: fasta_info(alignment), alignments)))
-    fasta_file = open("blast_result.fasta", "w")
-    fasta_file.write(content)
-    fasta_file.close()
-    return fasta_file.name
-
-
-def blast(input_fasta, db_path):
-    # Blast
-    blast_result = os.popen(f'blastp -query {input_fasta} -db {db_path} -outfmt 5')
-    blast_records = NCBIXML.read(blast_result)
-
-    sequence = []
-    with open(input_fasta, "r") as f:
-        for line in f.readlines():
-            if not line.startswith(">"):
-                sequence.append(line)
-    protein_chain = "".join(sequence).replace("\n", "")
-
-    # Mejores hits segun criterio
-    filtered_results = list(filter(
-        lambda alignment: meets_expected_criteria(protein_chain, alignment), blast_records.alignments
-    ))
-
-    # Crear Fasta con hits
-    return make_fasta(filtered_results)
+from blast import Blast
+from entrez_api_client import EntrezApiClient
 
 
 def map_uniprot_ids_to_entrez_ids(fasta_file):
@@ -121,20 +46,23 @@ def clustal(input_fasta):
 
 
 def fetch_cds_from_entrez(ids):
-    Entrez.email = 'juliancalvento@gmail.com'
     entrez_ids = list(map(lambda entrez_dict: entrez_dict['entrez_id'], ids))
-    fetch_response = Entrez.efetch(db="nucleotide", id=entrez_ids, rettype="gb", retmode="xml")
-    parsed_response = Entrez.read(fetch_response)
+    entrez_api_client = EntrezApiClient('juliancalvento@gmail.com')
+    entez_response = entrez_api_client.efetch(entrez_ids)
     # # del fetch response[0] sacar 'GBSeq_feature-table', y de la parte de CDS 'GBFeature_location', después cortar la secuencia desde inicio - 1 al fin
     # # La secuencia está en 'GBSeq_sequence'
-    return parsed_response
+    return entez_response
 
 
-if __name__ == '__main__':
-    fasta_file = blast('./query.fasta', '../swissprot/swissprot')
-
-    # Checkpoint
+def alignment_preparation(fasta_file):
     cd_hit_output_file = cd_hit(fasta_file)
     clustal_output = clustal(cd_hit_output_file)
     uniprot_to_entrez = map_uniprot_ids_to_entrez_ids(clustal_output)
     fetch_cds_from_entrez(uniprot_to_entrez)
+
+
+if __name__ == '__main__':
+    fasta_file = Blast().run('./query.fasta', '../swissprot/swissprot')
+
+    # Checkpoint
+    alignment_preparation(fasta_file)
